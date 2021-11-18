fd3b2cf571c0bdae4b4b7d64eff7bec0da2e7070
Tom <whynotavailable@protonmail.com>
2021-11-18 10:28:39 -0500

    Mouse movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,18 +1,62 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+using Extensions;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private InputAction lookAction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<PlayerInput>().actions["Click"].performed += ctx => Clicked();
+        lookAction = GetComponent<PlayerInput>().actions["Look"];
+    }
+
+    // TODO: save the movement from the click and do it in a FixedUpdate
+    void Clicked()
+    {
+        // Get the current mouse position
+        var pos = Camera.main.ScreenToWorldPoint(lookAction.ReadValue<Vector2>());
+
+        // Clamp the difference
+        var diff = (gameObject.transform.position - pos.Round().WithZ(0)).Clamp();
+
+        // Apply the difference to the character
+        var target = gameObject.transform.position - diff;
+
+        var collider = Physics2D.OverlapCircle(target, .1f);
+
+        var current = gameObject.transform.position;
+
+        if (collider != null)
+        {
+            Debug.Log(collider);
+            var targetX = target.WithY(current.y);
+            var targetY = target.WithX(current.x);
+
+            if (Physics2D.OverlapCircle(targetX, .1f) == null)
+            {
+                target = targetX;
+            }
+            else if (Physics2D.OverlapCircle(targetY, .1f) == null)
+            {
+                target = targetY;
+            }
+            else
+            {
+                target = gameObject.transform.position;
+            }
+        }
+
+        gameObject.transform.position = target;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/VectorExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class VectorExtensions
+    {
+        public static Vector3 WithZ(this Vector3 vector, float z)
+        {
+            return new Vector3(vector.x, vector.y, z);
+        }
+
+        public static Vector3 WithX(this Vector3 vector, float x)
+        {
+            return new Vector3(x, vector.y, vector.z);
+        }
+
+        public static Vector3 WithY(this Vector3 vector, float y)
+        {
+            return new Vector3(vector.x, y, vector.z);
+        }
+
+        public static Vector3 Round(this Vector3 vector)
+        {
+            return new Vector3(Mathf.Round(vector.x), Mathf.Round(vector.y), Mathf.Round(vector.z));
+        }
+
+        public static Vector3 Clamp(this Vector3 vector, float min = -1, float max = 1)
+        {
+            // don't need to clamp z and no-one gives a shit about it
+            return new Vector3(Mathf.Clamp(vector.x, min, max), Mathf.Clamp(vector.y, min, max), vector.z);
+        }
+    }
+}
